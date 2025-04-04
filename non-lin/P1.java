062d3e3645da16963292337f49d30072b6ff0e24
Amogh <142930584+amogh344@users.noreply.github.com>
2025-04-04 11:33:23 +0530

    added p1

---
+++ non-lin/P1.java
@@ -0,0 +1,23 @@
+
+class Node
+{
+    Node right,left;
+    int data;
+    Node n = new Node(left,data,right)
+}
+
+class P1
+{
+    public static void main(String []args)
+    {
+        int[] a={10,20,30,40,50} 
+        void buildtree(int a[] ,int i)
+        {
+
+        }
+
+        buildtree(a,0);
+    
+    }
+}
+
